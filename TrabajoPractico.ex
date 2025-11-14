a1e0f978dfd9ab43a40f4fd5045f958db148314d
agustin22337 <37782178+agustin22337@users.noreply.github.com>
2025-11-14 10:19:43 -0300

    dominios

--- TrabajoPractico.ex
+++ TrabajoPractico.ex
@@ -1,5 +1,6 @@
 defmodule TrabajoPractico do
   # 16 Escriba una función que devuelva el mínimo elemento de una lista.
+  # todas las listas de un mismo tipo que puedan compararse mediante el <
   def minimo(lista) do
     if tl(lista) == [] do
       hd(lista)
@@ -13,6 +14,7 @@ defmodule TrabajoPractico do
   end
 
   # 17 Escriba una función que devuelva el máximo elemento de una lista.
+    # todas las listas de un mismo tipo que puedan compararse mediante el >
   def maximo(lista) do
     if tl(lista) == [] do
       hd(lista)
@@ -26,14 +28,15 @@ defmodule TrabajoPractico do
   end
 
   # 18 Defina una función que tome una lista de números y devuelva una 3-upla formada por el promedio, el máximo y el mínimo de la lista.
-  def sumarLista(lista) do
+  # el dominio es todos los numeros reales, y la lista debe tener al menos un elemento
+  def sumarLista(lista) do [1,2,3]
     if lista == [] do
       0
     else
       hd(lista) + sumarLista(tl(lista))
     end
   end
-
+# dominio: cualquier lista con cualquier tipo de dato dentro
   def cantidadElementos(lista) do
     if lista == [] do
       0
@@ -41,18 +44,18 @@ defmodule TrabajoPractico do
       1 + cantidadElementos(tl(lista))
     end
   end
-
+# dominio: todos los reales, no se permiten listas vacias
   def promedio(lista) do
     sumarLista(lista) / cantidadElementos(lista)
   end
-
+# dominio todos los reales
   def procesarLista(lista) do
     {promedio(lista), maximo(lista), minimo(lista)}
   end
 
   # 28 Escriba una función que determine si una lista de {0,1}, donde cada 0 representa un paréntesis que abre y cada 1 un paréntesis que cierra, está balanceada (es decir, que cada 0 tiene su 1 correspondiente de acuerdo a las reglas que siguen los paréntesis). Ej: (001101) está balanceada y (1001010) no lo está.
-
-  def parentesisBalanceados(lista, n) do
+# dominio: los enteros 0 y 1
+  def parentesisBalanceados(lista) do
     if lista == [] do
       n == 0
     else
@@ -69,8 +72,9 @@ defmodule TrabajoPractico do
   end
 
 
-#41 Defina una función llamada “Select” que devuelva la lista de elementos que cumplen con una determinada condición.
 
+#41 Defina una función llamada “Select” que devuelva la lista de elementos que cumplen con una determinada condición.
+# Cualquier lista (posiblemente vacia) cuyos elementos puedan ser evaluados por la funcion condicion que se recibe
 def select(lista, condicion) do
     if lista == [] do
         []
@@ -83,7 +87,7 @@ def select(lista, condicion) do
 end
 
 #42 Defina una función llamada “Map” o “Collect” que devuelva la lista de los resultados de aplicar una función que se pasa como parámetro a cada elemento de la lista de entrada.
-
+#Cualquier lista (posiblemente vacia) cuyos elementos puedan ser evaluados por la funcion condicion que se recibe
 def map(lista, funcion)do
      if lista == [] do
         []
@@ -94,9 +98,9 @@ end
 
 end
 
-
+IO.puts(TrabajoPractico.minimo(['a','b','c']))
 IO.inspect(TrabajoPractico.procesarLista([1, 2, 3]))
 IO.puts(TrabajoPractico.maximo([5, 7, 8, 4]))
-IO.puts(TrabajoPractico.parentesisBalanceados([0, 0, 1, 1, 0, 1, 1], 0))
+IO.puts(TrabajoPractico.parentesisBalanceados([0, 0, 1, 1, 0, 1, 1],0))
 IO.inspect(TrabajoPractico.select([1,2,3,4,5], fn x -> rem(x,2) == 0 end))
 IO.inspect(TrabajoPractico.map([1,2,3,4,5], fn x -> x*2 end))
