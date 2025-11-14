4408a833d4d0e6cf4bf61220fe3d856d8b485dbc
agustin22337 <37782178+agustin22337@users.noreply.github.com>
2025-11-14 17:36:45 -0300

    Update TrabajoPractico.ex

--- TrabajoPractico.ex
+++ TrabajoPractico.ex
@@ -14,7 +14,11 @@ defmodule TrabajoPractico do
   end
 
   # 17 Escriba una función que devuelva el máximo elemento de una lista.
+<<<<<<< Updated upstream
     # todas las listas no vacias de un mismo tipo que puedan compararse mediante el >
+=======
+  # todas las listas de un mismo tipo que puedan compararse mediante el >
+>>>>>>> Stashed changes
   def maximo(lista) do
     if tl(lista) == [] do
       hd(lista)
@@ -30,13 +34,19 @@ defmodule TrabajoPractico do
   # 18 Defina una función que tome una lista de números y devuelva una 3-upla formada por el promedio, el máximo y el mínimo de la lista.
   # el dominio es todos los numeros reales, y la lista debe tener al menos un elemento
   def sumarLista(lista) do
+<<<<<<< Updated upstream
     if tl(lista) == [] do
       hd(lista)
+=======
+    if lista == [] do
+      0
+>>>>>>> Stashed changes
     else
       hd(lista) + sumarLista(tl(lista))
     end
   end
-# dominio: cualquier lista con cualquier tipo de dato dentro
+
+  # dominio: cualquier lista con cualquier tipo de dato dentro
   def cantidadElementos(lista) do
     if lista == [] do
       0
@@ -44,18 +54,29 @@ defmodule TrabajoPractico do
       1 + cantidadElementos(tl(lista))
     end
   end
-# dominio: todos los reales, no se permiten listas vacias
+
+  # dominio: todos los reales, no se permiten listas vacias
   def promedio(lista) do
     sumarLista(lista) / cantidadElementos(lista)
   end
+<<<<<<< Updated upstream
 # dominio todos los reales, no se permiten listas vacias
+=======
+
+  # dominio todos los reales
+>>>>>>> Stashed changes
   def procesarLista(lista) do
     {promedio(lista), maximo(lista), minimo(lista)}
   end
 
   # 28 Escriba una función que determine si una lista de {0,1}, donde cada 0 representa un paréntesis que abre y cada 1 un paréntesis que cierra, está balanceada (es decir, que cada 0 tiene su 1 correspondiente de acuerdo a las reglas que siguen los paréntesis). Ej: (001101) está balanceada y (1001010) no lo está.
+<<<<<<< Updated upstream
 # dominio: los enteros 0 y 1
   def parentesisBalanceados(lista, n) do
+=======
+  # dominio: los enteros 0 y 1
+  def procesarParentesis(lista, n)do
+>>>>>>> Stashed changes
     if lista == [] do
       n == 0
     else
@@ -63,29 +84,45 @@ defmodule TrabajoPractico do
         false
       else
         if hd(lista) == 0 do
-          parentesisBalanceados(tl(lista), n + 1)
+          procesarParentesis(tl(lista), n + 1)
         else
-          parentesisBalanceados(tl(lista), n - 1)
+          procesarParentesis(tl(lista), n - 1)
         end
       end
     end
   end
 
 
+  def parentesisBalanceados(lista) do
+    procesarParentesis(lista,0);
+  end
 
-#41 Defina una función llamada “Select” que devuelva la lista de elementos que cumplen con una determinada condición.
-# Cualquier lista (posiblemente vacia) cuyos elementos puedan ser evaluados por la funcion condicion que se recibe
-def select(lista, condicion) do
+  # 41 Defina una función llamada “Select” que devuelva la lista de elementos que cumplen con una determinada condición.
+  # Cualquier lista (posiblemente vacia) cuyos elementos puedan ser evaluados por la funcion condicion que se recibe
+  def select(lista, condicion) do
     if lista == [] do
-        []
-    else if condicion.(hd(lista)) do
+      []
+    else
+      if condicion.(hd(lista)) do
         [hd(lista) | select(tl(lista), condicion)]
-    else select(tl(lista), condicion)
+      else
+        select(tl(lista), condicion)
+      end
     end
-    end
+  end
 
+  # 42 Defina una función llamada “Map” o “Collect” que devuelva la lista de los resultados de aplicar una función que se pasa como parámetro a cada elemento de la lista de entrada.
+  # Cualquier lista (posiblemente vacia) cuyos elementos puedan ser evaluados por la funcion condicion que se recibe
+  def map(lista, funcion) do
+    if lista == [] do
+      []
+    else
+      [funcion.(hd(lista)) | map(tl(lista), funcion)]
+    end
+  end
 end
 
+<<<<<<< Updated upstream
 #42 Defina una función llamada “Map” o “Collect” que devuelva la lista de los resultados de aplicar una función que se pasa como parámetro a cada elemento de la lista de entrada.
 #Cualquier lista (posiblemente vacia) cuyos elementos puedan ser evaluados por la funcion que se recibe
 def map(lista, funcion)do
@@ -104,3 +141,11 @@ IO.puts(TrabajoPractico.maximo([5, 7, 8, 4]))
 IO.puts(TrabajoPractico.parentesisBalanceados([0, 0, 1, 1, 0, 1, 1],0))
 IO.inspect(TrabajoPractico.select([1,2,3,4,5], fn x -> rem(x,2) == 0 end))
 IO.inspect(TrabajoPractico.map([1,2,3,4,5], fn x -> x*2 end))
+=======
+# IO.puts(TrabajoPractico.minimo(['a', 'b', 'c']))
+# IO.inspect(TrabajoPractico.procesarLista([1, 2, 3]))
+# IO.puts(TrabajoPractico.maximo([5, 7, 8, 4]))
+IO.puts(TrabajoPractico.parentesisBalanceados([0,  0,1, 1,0, 1]))
+# IO.inspect(TrabajoPractico.select([1, 2, 3, 4, 5], fn x -> rem(x, 2) == 0 end))
+IO.inspect(TrabajoPractico.map([1, 2, 3, 4, 5], fn x -> x * 2 end))
+>>>>>>> Stashed changes
